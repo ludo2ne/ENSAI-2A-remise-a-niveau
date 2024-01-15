f62f08f3f400cf7f85c6b119c1759c0f268762a7
Ludovic Deneuville <ludovic.deneuville@ensai.fr>
2024-01-15 16:02:44 +0100

    agrs and kwargs

--- Outils/Arborescence/1A_create_folders.sh
+++ Outils/Arborescence/1A_create_folders.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-input_file="folders.csv"
-#directory="$HOME/Cours2A"
-directory="/p/Cours2A"
+input_file="folders_1A.csv"
+#directory="$HOME/Cours1A"
+directory="/p/Cours1A"
 
 mkdir -p "$directory"
 
 # Read the CSV file, skip the headers, and create the folders
-while IFS=';' read -r folder_name td tp; do
+while IFS=';' read -r folder_name td tp end; do
     # Create the main folder
     mkdir -p "$directory/$folder_name"
     mkdir -p "$directory/$folder_name/Annales"
