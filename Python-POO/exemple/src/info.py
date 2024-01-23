b2f0dcaa367af34f1f6f36e047fe69824c24bbd8
Ludovic Deneuville <ludovic.deneuville@ensai.fr>
2024-01-23 17:18:28 +0100

    Built site for gh-pages

--- Python-POO/exemple/src/info.py
+++
@@ -1,6 +0,0 @@
-import platform
-
-print("Python Version:", platform.python_version())
-print("Python Implementation:", platform.python_implementation())
-print("Operating System:", platform.system())
-print("Processor Architecture:", platform.machine())
